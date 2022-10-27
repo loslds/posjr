32e71c73019ee85e37fbe6c823d249d767be0d28
loslds <los.sbrissa@gmail.com>
2022-10-27 20:06:02 -0300

    aa

--- src/pages/AccesFuncs.tsx
+++ src/pages/AccesFuncs.tsx
@@ -1,45 +1,43 @@
-type PropsStrValue = {
-  strvalue: string;
-  rtn: any;
-  forca: number;
+type PropsChkPoint = {
+  str: [];
 };
-export function valorForca({ strvalue, forca, rtn }: PropsStrValue) {
+export function CheckPointPassword({ str }: PropsChkPoint) {
   // const [valueForca, setvalueForca] = useState(0);
-  var forca = 0;
-  if (strvalue.length <= 3) {
-    forca = 0; /** força fraca  */
-  }
-  if (strvalue.length >= 4 && strvalue.length <= 7) {
-    forca = forca + 10;
-  }
-  if (strvalue.length >= 7) {
-    forca = forca + 25;
-  }
-  if (strvalue.length >= 5 && strvalue.match(/[a-z] + /)) {
-    forca = forca + 10;
-  }
-  if (strvalue.length >= 6 && strvalue.match(/[A-Z] + /)) {
-    forca = forca + 20;
-  }
-  if (
-    strvalue.length >= 7 &&
-    strvalue.match(/[!*"#$%&'*+,-./:;<=>?@\^_`|~] + /)
-  ) {
-    forca = forca + 20;
-  }
+  const istr = str;
+  const forca = istr.length * 1;
 
+  // if (str.length > 0) {
+  //   forca = str.length * 1;
+  // }
+  // if (str.length === 2) {
+  //   forca = forca + 10;
+  // }
+
+  // if (str.length >= 4 && str.length <= 7) {
+  //   forca = forca + 10;
+  // }
+  // if (str.length >= 7) {
+  //   forca = forca + 25;
+  // }
+  // if (str.length >= 5 && str.match(/[a-z] + /)) {
+  //   forca = forca + 10;
+  // }
+  // if (str.length >= 6 && str.match(/[A-Z] + /)) {
+  //   forca = forca + 20;
+  // }
+  // if (str.length >= 7 && str.match(/[!*"#$%&'*+,-./:;<=>?@\^_`|~] + /)) {
+  //   forca = forca + 20;
+  // }
   if (forca < 30) {
-    rtn = 'FRACA...';
+    return 'FRACA...';
   }
   if (forca > 30 && forca < 50) {
-    rtn = 'MÉDIA...';
+    return 'MÉDIA...';
   }
   if (forca >= 50 && forca < 70) {
-    rtn = 'FORTE...';
+    return 'FORTE...';
   }
   if (forca >= 70 && forca < 100) {
-    rtn = 'EXCELENTE...';
+    return 'EXCELENTE...';
   }
-  //, console.log('rtn : ' + rtn);
-  return String(rtn);
 }
