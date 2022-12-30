868b3fd65b07eb61914e67bf829185dd86b7c6ce
loslds <los.sbrissa@gmail.com>
2022-12-30 11:30:23 -0300

    1

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -59,7 +59,6 @@ export const HomePage = () => {
                 img={loginbrc1}
                 title={'Acesso...'}
                 onClick={goto('/access')}
-
               />
             </ContainerPageFlexWidth>
           </ContainerPageDoubleFlex>
