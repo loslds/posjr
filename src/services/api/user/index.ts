82d7816a1f3fffc9c70a3ab10d3cb78a962e4763
loslds <los.sbrissa@gmail.com>
2023-01-19 18:43:18 -0300

    err

--- src/services/api/user/index.ts
+++ src/services/api/user/index.ts
@@ -30,27 +30,54 @@ export async function getUser(id: number): Promise<IResponseUser> {
   return { success: !!found, user: found };
 }
 
+//export async function getLvInmPinlUsers(
+//  { level, idname, pin }: UserFilter = {},
+//  { order = 'asc', orderBy = 'level' }: UserOrder = {}
+//  ): Promise<IResponseUserList> {
+//    if (level !== 0 && idname !== '' && pin !== '') {
+//    const found = userData.users.find(
+//      f => f.level === level && f.idname === idname && f.pin === pin
+//    );
+//
+//    await wait(800);
+//    return userData;
+//  }
+//}
+
 /** Adquiri um usuário pelo : id,level,idname,pin */
-export async function getLvNmPnUsers(
-  { id, level, idname, pin }: UserFilter = {},
-  { order = 'asc', orderBy = 'id' }: UserOrder = {}
+
+export async function getLevelUsers(
+  { level }: UserFilter = {},
+  { order = 'asc', orderBy = 'level' }: UserOrder = {}
 ): Promise<IResponseUserList> {
-  if (id) {
-    userData.users = userData?.users?.filter(f => f.id === id);
-  }
   if (level) {
     userData.users = userData?.users?.filter(f => f.level === level);
-  }
-  if (idname) {
-    userData.users = userData?.users?.filter(f => f.idname === idname);
-  }
-  if (pin) {
-    userData.users = userData?.users?.filter(f => f.idname === pin);
   }
 
   userData.users = userData?.users?.sort(compareValues(orderBy, order));
 
   await wait(200);
+
+  console.log('level : ', level);
   console.log('userData : ', userData.users);
+
+  return userData;
+}
+
+export async function getIdnamelUsers(
+  { idname }: UserFilter = {},
+  { order = 'asc', orderBy = 'idname' }: UserOrder = {}
+): Promise<IResponseUserList> {
+  if (idname) {
+    userData.users = userData?.users?.filter(f => f.idname === idname);
+  }
+
+  userData.users = userData?.users?.sort(compareValues(orderBy, order));
+
+  await wait(200);
+
+  console.log('level : ', idname);
+  console.log('userData : ', userData.users);
+
   return userData;
 }
